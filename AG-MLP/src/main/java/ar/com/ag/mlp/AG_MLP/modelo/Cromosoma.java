6715b245d1246a9047c821927d607822071ad551
PabloHernanSuarez <pablo.hernan.suarez@gmail.com>
2019-11-15 16:12:31 -0300

    Función de fitness lista

--- AG-MLP/src/main/java/ar/com/ag/mlp/AG_MLP/modelo/Cromosoma.java
+++ AG-MLP/src/main/java/ar/com/ag/mlp/AG_MLP/modelo/Cromosoma.java
@@ -1,73 +1,116 @@
 package ar.com.ag.mlp.AG_MLP.modelo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Cromosoma {
 	
-	private ArrayList<Gene> genes;
-	// apSesgo= wb
-	private float apSesgo;
-	private int nMax;
-	private float aptitud;
-	private float relAptitud;
-	private float acumAptitud;
-	
-	public Cromosoma() {
-		super();		
-	}
+    private ArrayList<Gene> genes;
+    // apSesgo= wb
+    private double apSesgo;
+    private int nMax;
+    private double aptitud;
+    private double relAptitud;
+    private double acumAptitud;
 
-	public ArrayList<Gene> getGenes() {
-		return genes;
-	}
+    public Cromosoma() {
+        super();		
+    }
 
-	public void setGenes(ArrayList<Gene> genes) {
-		this.genes = genes;
-	}
+    public ArrayList<Gene> getGenes() {
+        return genes;
+    }
 
-	public float getApSesgo() {
-		return apSesgo;
-	}
+    public void setGenes(ArrayList<Gene> genes) {
+        this.genes = genes;
+    }
 
-	public void setApSesgo(float apSesgo) {
-		this.apSesgo = apSesgo;
-	}
+    public double getApSesgo() {
+        return apSesgo;
+    }
 
-	public int getnMax() {
-		return nMax;
-	}
+    public void setApSesgo(double apSesgo) {
+        this.apSesgo = apSesgo;
+    }
 
-	public void setnMax(int nMax) {
-		this.nMax = nMax;
-	}
+    public int getnMax() {
+        return nMax;
+    }
 
-	public float getAptitud() {
-		return aptitud;
-	}
+    public void setnMax(int nMax) {
+        this.nMax = nMax;
+    }
 
-	public void setAptitud(float aptitud) {
-		this.aptitud = aptitud;
-	}
+    public double getAptitud() {
+        return aptitud;
+    }
 
-	public float getRelAptitud() {
-		return relAptitud;
-	}
+    public void setAptitud(double aptitud) {
+        this.aptitud = aptitud;
+    }
 
-	public void setRelAptitud(float relAptitud) {
-		this.relAptitud = relAptitud;
-	}
+    public double getRelAptitud() {
+        return relAptitud;
+    }
 
-	public float getAcumAptitud() {
-		return acumAptitud;
-	}
+    public void setRelAptitud(double relAptitud) {
+        this.relAptitud = relAptitud;
+    }
 
-	public void setAcumAptitud(float acumAptitud) {
-		this.acumAptitud = acumAptitud;
-	}
+    public double getAcumAptitud() {
+        return acumAptitud;
+    }
 
-	@Override
-	public String toString() {
-		return "Cromosoma [genes=" + genes + ", apSesgo=" + apSesgo + ", nMax=" + nMax + ", aptitud=" + aptitud
-				+ ", relAptitud=" + relAptitud + ", acumAptitud=" + acumAptitud + "]";
-	}
-	
+    public void setAcumAptitud(double acumAptitud) {
+        this.acumAptitud = acumAptitud;
+    }
+
+    public double calcularFuncionDeEvaluacion() {
+
+        double cantidadPatrones = 33;
+        double sumaErroresMedios = 0;
+
+        try {
+            File myObj = new File("C:\\Users\\Pablo\\Desktop\\dataset-neuroph-procesado.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] arreglo = data.split("\t");
+                ArrayList<Double> entradas = new ArrayList();
+                
+                for (int i = 0; i < 7; i++) {
+                    entradas.add(Double.parseDouble(arreglo[i]));
+                }    
+                
+                double entradaNodoSalida = this.getApSesgo();
+
+                for (Gene g : this.getGenes()) {
+                    entradaNodoSalida += g.getWs() * g.calcularValorDeSalida(entradas);
+                }
+
+                double salidaNodoSalida = 1 /(1 + Math.exp(-entradaNodoSalida));
+                double salidaEsperada = Double.parseDouble(arreglo[7]);
+                
+                System.out.println("Salida nodo: " + salidaNodoSalida);
+                System.out.println("Salida esperada: " + salidaEsperada);
+                
+                sumaErroresMedios += Math.pow((salidaEsperada - salidaNodoSalida), 2);
+            }
+            myReader.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        
+        return (sumaErroresMedios / cantidadPatrones);
+    }
+
+    @Override
+    public String toString() {
+        return "Cromosoma [genes=" + genes + ", apSesgo=" + apSesgo + ", nMax=" + nMax + ", aptitud=" + aptitud
+                            + ", relAptitud=" + relAptitud + ", acumAptitud=" + acumAptitud + "]";
+    }
 }
--- AG-MLP/src/main/java/ar/com/ag/mlp/AG_MLP/modelo/Gene.java
+++ AG-MLP/src/main/java/ar/com/ag/mlp/AG_MLP/modelo/Gene.java
@@ -1,102 +1,115 @@
 package ar.com.ag.mlp.AG_MLP.modelo;
 
+import java.util.ArrayList;
+
 public class Gene {
 
-	private float wb, w1, w2, w3, w4, w5, w6, w7, ws;
+    private double wb, w1, w2, w3, w4, w5, w6, w7, ws;
 
-	public Gene(float wb, float w1, float w2, float w3, float w4, float w5, float w6, float w7, float ws) {
-		super();
-		this.wb = wb;
-		this.w1 = w1;
-		this.w2 = w2;
-		this.w3 = w3;
-		this.w4 = w4;
-		this.w5 = w5;
-		this.w6 = w6;
-		this.w7 = w7;
-		this.ws = ws;
-	}
+    public Gene(double wb, double w1, double w2, double w3, double w4, double w5, double w6, double w7, double ws) {
+        super();
+        this.wb = wb;
+        this.w1 = w1;
+        this.w2 = w2;
+        this.w3 = w3;
+        this.w4 = w4;
+        this.w5 = w5;
+        this.w6 = w6;
+        this.w7 = w7;
+        this.ws = ws;
+    }
 
-	public Gene() {
-		super();		
-	}
+    public Gene() {
+        super();		
+    }
 
-	public float getWb() {
-		return wb;
-	}
+    public double getWb() {
+        return wb;
+    }
 
-	public void setWb(float wb) {
-		this.wb = wb;
-	}
+    public void setWb(double wb) {
+        this.wb = wb;
+    }
 
-	public float getW1() {
-		return w1;
-	}
+    public double getW1() {
+        return w1;
+    }
 
-	public void setW1(float w1) {
-		this.w1 = w1;
-	}
+    public void setW1(double w1) {
+        this.w1 = w1;
+    }
 
-	public float getW2() {
-		return w2;
-	}
+    public double getW2() {
+        return w2;
+    }
 
-	public void setW2(float w2) {
-		this.w2 = w2;
-	}
+    public void setW2(double w2) {
+        this.w2 = w2;
+    }
 
-	public float getW3() {
-		return w3;
-	}
+    public double getW3() {
+        return w3;
+    }
 
-	public void setW3(float w3) {
-		this.w3 = w3;
-	}
+    public void setW3(double w3) {
+        this.w3 = w3;
+    }
 
-	public float getW4() {
-		return w4;
-	}
+    public double getW4() {
+        return w4;
+    }
 
-	public void setW4(float w4) {
-		this.w4 = w4;
-	}
+    public void setW4(double w4) {
+        this.w4 = w4;
+    }
 
-	public float getW5() {
-		return w5;
-	}
+    public double getW5() {
+        return w5;
+    }
 
-	public void setW5(float w5) {
-		this.w5 = w5;
-	}
+    public void setW5(double w5) {
+        this.w5 = w5;
+    }
 
-	public float getW6() {
-		return w6;
-	}
+    public double getW6() {
+        return w6;
+    }
 
-	public void setW6(float w6) {
-		this.w6 = w6;
-	}
+    public void setW6(double w6) {
+        this.w6 = w6;
+    }
 
-	public float getW7() {
-		return w7;
-	}
+    public double getW7() {
+        return w7;
+    }
 
-	public void setW7(float w7) {
-		this.w7 = w7;
-	}
+    public void setW7(double w7) {
+        this.w7 = w7;
+    }
 
-	public float getWs() {
-		return ws;
-	}
+    public double getWs() {
+        return ws;
+    }
 
-	public void setWs(float ws) {
-		this.ws = ws;
-	}
+    public void setWs(double ws) {
+        this.ws = ws;
+    }
 
-	@Override
-	public String toString() {
-		return "Gene [wb=" + wb + ", w1=" + w1 + ", w2=" + w2 + ", w3=" + w3 + ", w4=" + w4 + ", w5=" + w5 + ", w6="
-				+ w6 + ", w7=" + w7 + ", ws=" + ws + "]";
-	}
-	
+    public double calcularValorDeSalida(ArrayList<Double> entradas) {
+
+        double entradaNodo = 0;
+        double salidaNodo = 0;
+
+        entradaNodo = this.getWb() + this.getW1() * entradas.get(0) + this.getW2() * entradas.get(1)
+                + this.getW3() * entradas.get(2) + this.getW4() * entradas.get(3) + this.getW5() * entradas.get(4)
+                + this.getW6() * entradas.get(5) + this.getW7() * entradas.get(6);
+        salidaNodo = 1 / (1 + Math.exp(-entradaNodo));
+        return salidaNodo;
+    }
+
+    @Override
+    public String toString() {
+        return "Gene [wb=" + wb + ", w1=" + w1 + ", w2=" + w2 + ", w3=" + w3 + ", w4=" + w4 + ", w5=" + w5 + ", w6="
+                            + w6 + ", w7=" + w7 + ", ws=" + ws + "]";
+    }
 }
